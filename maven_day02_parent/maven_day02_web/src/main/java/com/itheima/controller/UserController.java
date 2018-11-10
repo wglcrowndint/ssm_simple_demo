b7a415886cac0aae3d830b196796bdf080ab67fe
crowndint <crowndint@163.com>
2018-11-10 10:55:50 +0800

    dev 添加UserController

---
+++ maven_day02_parent/maven_day02_web/src/main/java/com/itheima/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.itheima.controller;
+
+import com.itheima.domain.User;
+
+/**
+ * Created by crowndint on 2018/11/10.
+ */
+public class UserController {
+
+    public void add(User user) {
+
+
+    }
+}
