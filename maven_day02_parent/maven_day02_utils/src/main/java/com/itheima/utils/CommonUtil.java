fe2fa6521e5509c1625aac426f505513259efdcf
crowndint <crowndint@163.com>
2018-11-10 10:41:55 +0800

    dev 分支开发了工具模块

---
+++ maven_day02_parent/maven_day02_utils/src/main/java/com/itheima/utils/CommonUtil.java
@@ -0,0 +1,12 @@
+package com.itheima.utils;
+
+/**
+ * Created by crowndint on 2018/11/10.
+ */
+public class CommonUtil {
+
+    public boolean stringIsBlank(String str) {
+
+        return str==null && str.length()<1;
+    }
+}
