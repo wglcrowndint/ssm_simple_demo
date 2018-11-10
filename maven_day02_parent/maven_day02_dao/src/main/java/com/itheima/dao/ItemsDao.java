394bdee575e99ab8d8c6727cb3e0c90af9915490
crowndint <crowndint@163.com>
2018-11-10 10:30:42 +0800

    ItemsDao添加了findAll方法

--- maven_day02_parent/maven_day02_dao/src/main/java/com/itheima/dao/ItemsDao.java
+++ maven_day02_parent/maven_day02_dao/src/main/java/com/itheima/dao/ItemsDao.java
@@ -2,6 +2,9 @@ package com.itheima.dao;
 
 import com.itheima.domain.Items;
 
+import java.util.List;
+
 public interface ItemsDao {
     public Items findById(Integer id);
+    public List<Items> findAll();
 }
